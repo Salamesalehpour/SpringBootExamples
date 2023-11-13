5845d8ef21676b76019af86eeb3b9304632f7b6f
Salam Salehpour <salamesalehpour@gmail.com>
2023-11-13 18:41:49 +0330

    Read data from app.props config

--- src/main/java/com/example/demo/rest/FunRestController.java
+++ src/main/java/com/example/demo/rest/FunRestController.java
@@ -1,13 +1,16 @@
 package com.example.demo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+    @Value("${helloWorldMessage}")
+    private String message;
     @GetMapping("/")
     public String sayHello(){
-        return "Hello World";
+        return message;
     }
 }
